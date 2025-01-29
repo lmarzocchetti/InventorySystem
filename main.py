5fafed1deb5271c996049fd5342c1da854ed0286
lmarzocchetti <lorenzomarzocchetti@gmail.com>
2025-01-29 17:31:10 +0100

    Implemented the simulation of the warehouse

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
+import random
+
+import simpy
+
+from src.warehouse import Warehouse
+import simulation_data as sim_data
+
 def main():
-    pass
+    random.seed(42)
+    env: simpy.Environment = simpy.Environment()
+    warehouse: Warehouse = Warehouse(
+        env,
+        sim_data.simulation_parameters,
+        [sim_data.first_product, sim_data.second_product]
+    )
+    env.run(until=24*50) # 50 giorni
+
+    print(warehouse.total_cost)
+
 
 if __name__ == "__main__":
     main()
--- src/Warehouse.py
+++ src/Warehouse.py
@@ -1,21 +1,35 @@
 import simpy
 import random
+import statistics
 
 from collections import defaultdict
 
-from utils import SimulationParaters, Product
+from .utils import SimulationParaters, Product
 
+# TODO: inventory level separated by product, so not assuming that every product occupy 1 slot
+# TODO: s_min and s_max customizable for every product
 class Warehouse:
     def __init__(
         self,
         env: simpy.Environment,
         parameters: SimulationParaters,
         products: list[Product],
-        inventory_level: float = 60,
-        inventory_check_interval: float = 1,
+        total_inventory_level_per_product: float = 60,
+        inventory_check_interval: float = 24, # FIXME: 24 ore
         s_max: float = 40,
         s_min: float = 20
     ) -> None:
+        """Warehouse that stores a fixed amount of products
+
+        Args:
+            env (simpy.Environment): Simpy environment
+            parameters (SimulationParaters): Simulation Parameters (see the class)
+            products (list[Product]): List of Products that are stored in the warehouse
+            total_inventory_level (float, optional): Amount of total products that can be stored in the warehouse(assuming all products occupy the same space). Defaults to 60.
+            inventory_check_interval (float, optional): Amount of time that we check the inventory and reorder products. Defaults to 1.
+            s_max (float, optional): Defaults to 40.
+            s_min (float, optional): Defaults to 20.
+        """ 
         self.env = env
         self.demand_inter_arrival_mean_time: float = parameters.demand_inter_arrival_mean_time
         self.order_setup_cost: float = parameters.order_setup_cost
@@ -23,15 +37,91 @@ class Warehouse:
         self.holding_cost: float = parameters.holding_cost
         self.shortage_cost: float = parameters.shortage_cost
         self.products: list[Product] = products
-        self.inventory_level: float = 60
+        self.total_inventory_level_per_product: float = total_inventory_level_per_product
         self.inventory_check_interval: float = inventory_check_interval
         self.s_max: float = s_max
         self.s_min: float = s_min
-    
+
+        # TODO: Try other alternative to this, or customizable by the user
+        # Initialize inventory levels per product at total_inventory_level_per_product / 2
+        self.current_inventory_level_products = [int(self.total_inventory_level_per_product/2) for i in range(len(self.products))]
+        assert len(self.current_inventory_level_products) == len(self.products)
+        print(self.current_inventory_level_products)
+
         self.total_order_cost = 0
-        self.inventory_history: dict[float, float] = defaultdict(float)
+        self.inventory_history: dict[int, dict[float, float]] = {idx:defaultdict(float) for idx in range(len(self.products))}
         
-        self.it: list[tuple[float, float]] = [(0, inventory_level)]
-        self.last_inventory_level: float = inventory_level
+        self.it: dict[int, list[tuple[int, int]]] = {idx:[(0, int(total_inventory_level_per_product/2))] for idx in range(len(self.products))}
+        self.last_inventory_level: dict[int, int] = {idx:int(total_inventory_level_per_product/2) for idx in range(len(self.products))}
         self.last_inventory_level_timestamp: float = 0.0
 
+        self.env.process(self.inventory_monitor())
+        self.env.process(self.demand_generator())
+
+    def inventory_level_setter(self, idx: int, operation, value):
+        self.inventory_history[idx][self.last_inventory_level[idx]] += self.env.now - self.last_inventory_level_timestamp
+
+        if operation == "add":
+            self.current_inventory_level_products[idx] += value
+        elif operation == "sub":
+            self.current_inventory_level_products[idx] -= value
+        else:
+            print(f"ERROR: Operation {operation} on setting the inventory level not supported")
+            exit(-1)
+        
+        self.it[idx].append((self.env.now, self.current_inventory_level_products[idx]))
+
+        self.last_inventory_level_timestamp = self.env.now
+        self.last_inventory_level[idx] = self.current_inventory_level_products[idx]
+
+    @property
+    def total_holding_cost(self) -> float:
+        acc = 0
+        for idx_prod in range(len(self.products)):
+            acc += sum(
+                max(inventory_level, 0) * duration * self.holding_cost
+                for inventory_level, duration in self.inventory_history[idx_prod].items())
+        return acc
+
+    @property
+    def total_shortage_cost(self) -> float:
+        acc = 0
+        for idx_prod in range(len(self.products)):
+            acc += sum(
+                max(-inventory_level, 0) * duration * self.shortage_cost
+                for inventory_level, duration in self.inventory_history[idx_prod].items())
+        return acc
+
+    @property
+    def total_cost(self) -> float:
+        return self.total_order_cost + self.total_holding_cost + self.total_shortage_cost
+   
+    def inventory_monitor(self):
+        while True:
+            yield self.env.timeout(self.inventory_check_interval)
+            for idx, product in enumerate(self.products):
+                if self.current_inventory_level_products[idx] < self.s_min:
+                    self.env.process(self.order_up_to_s_max(product, idx))
+
+    def order_up_to_s_max(self, product: Product, idx: int):
+        z = self.s_max - self.current_inventory_level_products[idx]
+        self.total_order_cost += (self.order_setup_cost + self.order_incremental_cost * z)
+        lead_time = random.uniform(product.lead_time_min, product.lead_time_max)
+        yield self.env.timeout(lead_time)
+        # self.current_inventory_level_products[idx] += z
+        self.inventory_level_setter(idx, "add", z)
+
+    def demand_generator(self):
+        while True:
+            demand_inter_arrival_time = random.expovariate(lambd=self.demand_inter_arrival_mean_time)
+            # For every product that warehouse handle
+            for idx, product in enumerate(self.products):
+                pop, weights = product.demand_distribution
+                demand_size = random.choices(pop, weights=weights, k=1)[0]
+
+                yield self.env.timeout(demand_inter_arrival_time)
+                # self.current_inventory_level_products[idx] -= demand_size
+                self.inventory_level_setter(idx, "sub", demand_size)
+
+    # TODO: Plot function
+                
